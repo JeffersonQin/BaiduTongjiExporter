619384cb3f8bd428c381bfec3c79cf11ac4bf695
gyro永不抽风 <1247006353@qq.com>
2022-07-16 10:13:17 +0800

    feat: structure delay

--- cli.py
+++ cli.py
@@ -16,44 +16,44 @@ def save_report(report, name, path):
 		json.dump(report, f, indent=4, ensure_ascii=False)
 
 
-def save_all(access_token, site_id, start_date, end_date, start_date2, end_date2, path):
-	time.sleep(10)
+def save_all(access_token, site_id, start_date, end_date, start_date2, end_date2, path, delay):
+	time.sleep(delay)
 	save_report(baidutongji.getTimeTrendRpt(access_token, site_id, start_date, end_date, baidutongji.TimeTrendRptMetrics().setAllTrue()), 'overview-getTimeTrendRpt', path)
 	print('Time Trend Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getDistrictRpt(access_token, site_id, start_date, end_date, baidutongji.DistrictRptMetrics().setAllTrue()), 'overview-getDistrictRpt', path)
 	print('District Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getCommonTrackRpt(access_token, site_id, start_date, end_date, baidutongji.CommonTrackRptMetrics().setAllTrue()), 'overview-getCommonTrackRpt', path)
 	print('Common Track Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getTrendTime(access_token, site_id, start_date, end_date, baidutongji.TrendTimeMetrics().setAllTrue(), start_date2, end_date2), 'trend-time', path)
 	print('Time Trend Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getSourceAll(access_token, site_id, start_date, end_date, baidutongji.SourceMetrics().setAllTrue(), start_date2, end_date2), 'source-all', path)
 	print('Source Report (All) Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getSourceEngine(access_token, site_id, start_date, end_date, baidutongji.SourceMetrics().setAllTrue(), start_date2, end_date2), 'source-engine', path)
 	print('Source Engine Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getSourceSearchword(access_token, site_id, start_date, end_date, baidutongji.SourceMetrics().setAllTrue(), start_date2, end_date2), 'source-searchword', path)
 	print('Source Searchword Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getSourceLink(access_token, site_id, start_date, end_date, baidutongji.SourceMetrics().setAllTrue(), start_date2, end_date2), 'source-link', path)
 	print('Source Link Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getVisitToppage(access_token, site_id, start_date, end_date, baidutongji.VisitToppageMetrics().setAllTrue(), start_date2, end_date2), 'visit-toppage', path)
 	print('Top Page Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getVisitLandingpage(access_token, site_id, start_date, end_date, baidutongji.VisitLandingpageMetrics().setAllTrue(), start_date2, end_date2), 'visit-landingpage', path)
 	print('Landing Page Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getVisitTopdomain(access_token, site_id, start_date, end_date, baidutongji.VisitTopdomainMetrics().setAllTrue(), start_date2, end_date2), 'visit-topdomain', path)
 	print('Top Domain Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getVisitDistrict(access_token, site_id, start_date, end_date, baidutongji.SourceMetrics().setAllTrue(), start_date2, end_date2), 'visit-district', path)
 	print('District Report Finished')
-	time.sleep(10)
+	time.sleep(delay)
 	save_report(baidutongji.getVisitWorld(access_token, site_id, start_date, end_date, baidutongji.SourceMetrics().setAllTrue(), start_date2, end_date2), 'visit-world', path)
 	print('World Report Finished')
 
@@ -62,7 +62,8 @@ def save_all(access_token, site_id, start_date, end_date, start_date2, end_date2
 @click.option('--access-token', '-t', default=None, help='Access token')
 @click.option('--path', '-p', default='.', help='Path to save report')
 @click.option('--date', '-d', default=None, help='Date to get report, in form of YYYY-MM-DD of Beijing time')
-def fetch(access_token, path, date):
+@click.option('--delay', '-D', default=15, help='Delay in seconds between each request')
+def fetch(access_token, path, date, delay):
 	if access_token is None:
 		access_token = os.environ.get('BAIDU_TONGJI_ACCESS_TOKEN')
 	if access_token is None:
@@ -116,14 +117,14 @@ def fetch(access_token, path, date):
 		print(f'Saving day report to {day_path}')
 		os.makedirs(day_path, exist_ok=True)
 
-		save_all(access_token, site_id, date, date, yesterday, yesterday, day_path)
+		save_all(access_token, site_id, date, date, yesterday, yesterday, day_path, delay)
 
 		if real_time:
 			# 实时访客
 			day_path = os.path.join(domain_path, f'day/{(date + datetime.timedelta(days=1)).strftime("%Y%m%d")}')
 			print(f'Saving realtime report to {day_path}')
 			os.makedirs(day_path, exist_ok=True)
-			time.sleep(15)
+			time.sleep(delay)
 			save_report(baidutongji.getTrendLatest(access_token, site_id, baidutongji.TrendLatestMetrics().setAllTrue()), f'trend-latest-{beijing_now.strftime("%H-%M-%S-UTC+8")}', day_path)
 
 		# week
@@ -131,16 +132,16 @@ def fetch(access_token, path, date):
 			week_path = os.path.join(domain_path, f'week/{this_week_start.strftime("%Y%m%d")}-{this_week_end.strftime("%Y%m%d")}')
 			print(f'Saving week report to {week_path}')
 			os.makedirs(week_path, exist_ok=True)
-			time.sleep(60)
-			save_all(access_token, site_id, this_week_start, this_week_end, last_week_start, last_week_end, week_path)
+			time.sleep(delay)
+			save_all(access_token, site_id, this_week_start, this_week_end, last_week_start, last_week_end, week_path, delay)
 
 		# month
 		if cron_month:
 			month_path = os.path.join(domain_path, f'month/{this_month_start.strftime("%Y%m%d")}-{this_month_end.strftime("%Y%m%d")}')
 			print(f'Saving month report to {month_path}')
 			os.makedirs(month_path, exist_ok=True)
-			time.sleep(60)
-			save_all(access_token, site_id, this_month_start, this_month_end, last_month_start, last_month_end, month_path)
+			time.sleep(delay)
+			save_all(access_token, site_id, this_month_start, this_month_end, last_month_start, last_month_end, month_path, delay)
 
 
 @cli.command()
@@ -149,10 +150,11 @@ def fetch(access_token, path, date):
 @click.option('--path', '-p', default='.', help='Path to save report')
 @click.argument('start_date', type=click.DateTime(formats=['%Y-%m-%d']))
 @click.argument('end_date', type=click.DateTime(formats=['%Y-%m-%d']))
-def fetch_all(ctx, access_token, path, start_date: click.DateTime, end_date: click.DateTime):
+@click.option('--delay', '-D', default=15, help='Delay in seconds between each request')
+def fetch_all(ctx, access_token, path, start_date: click.DateTime, end_date: click.DateTime, delay: int):
 	'''Fetch all history reports by indicating start date and end date in form of YYYY-MM-DD'''
 	while start_date <= end_date:
-		ctx.invoke(fetch, access_token=access_token, path=path, date=start_date.strftime('%Y-%m-%d'))
+		ctx.invoke(fetch, access_token=access_token, path=path, date=start_date.strftime('%Y-%m-%d'), delay=delay)
 		start_date = start_date + datetime.timedelta(days=1)
 
 
